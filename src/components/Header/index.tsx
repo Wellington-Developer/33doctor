98fb60fb036bada957c629314f1f905a648504e7
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-29 14:25:54 -0300

    chore: MUI lib has been added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,22 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Fade from '@mui/material/Fade';
+
 import './styles.css'
 import logo from '../../assets/logo.png';
 
 export const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <header className="header-container">
       <div className="left-side">
@@ -10,8 +25,32 @@ export const Header = () => {
       <div className="right-side">
         <nav>
           <ul>
-            <li><a href="#consultas">Consultas</a></li>
-            <li><a href="#exames">Exames</a></li>
+            <li>
+              <Button
+                id="fade-button"
+                className="button-header"
+                aria-controls={open ? 'fade-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+              >
+                Exames
+              </Button>
+              <Menu
+                id="fade-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'fade-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                TransitionComponent={Fade}
+              >
+                <MenuItem onClick={handleClose}>Cardiologista</MenuItem>
+                <MenuItem onClick={handleClose}>Clínico Geral</MenuItem>
+                <MenuItem onClick={handleClose}>Dermatologista</MenuItem>
+              </Menu>
+            </li>
           </ul>
         </nav>
       </div>

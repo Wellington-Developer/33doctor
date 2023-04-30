cf3ad54c27d982703cc00e74286ee50f263f707e
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-30 12:53:41 -0300

    feat: filter titles has been added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 // Aos
 import Aos from 'aos';
 import 'aos/dist/aos.css'
+import { Filter } from './components/Filter';
 
 function App() {
   useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
       <Header />
       <Intro />
+      <Filter />
     </div>
   );
 }
---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css';
+
+export const Filter = () => {
+  return (
+    <div className="filter-container">
+      <div className="titles">
+        <h1>A partir de R$ xx,xx</h1>
+        <div className="title">
+          <a href="#">Consutlas</a>
+          <a href="#">Oftalmo</a>
+          <a href="#">Imagem</a>
+          <a href="#">Cardiológico</a>
+          <a href="#">Laboratório</a>
+        </div>
+      </div>
+    </div>
+  )
+}

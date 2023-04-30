76d119fb3c9927fb0806b1dbc5f59df0258620b4
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-30 15:13:42 -0300

    feat: adding a filter right in filter container

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,5 +1,8 @@
 import './styles.css';
 
+// MUI icons
+import SearchIcon from '@mui/icons-material/Search';
+
 export const Filter = () => {
   return (
     <div className="filter-container">
@@ -11,6 +14,22 @@ export const Filter = () => {
           <a href="https://www.google.com.br/">Imagem</a>
           <a href="https://www.google.com.br/">Cardiológico</a>
           <a href="https://www.google.com.br/">Laboratório</a>
+        </div>
+      </div>
+
+      <div className="filter">
+        <div className="filter-input">
+          <input type="text" placeholder="Do que você precisa?">
+            
+          </input>
+          <div className="search-icon">
+            <SearchIcon />
+          </div>
+        </div>
+
+        <div className="filter-right">
+          <h1>Preço</h1>
+          <h1>Localidade</h1>
         </div>
       </div>
     </div>

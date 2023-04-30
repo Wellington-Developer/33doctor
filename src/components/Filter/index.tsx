fb70d0747aeae1cf5c3b5e4b0851e8b5478dd4bb
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-30 15:42:09 -0300

    chore: adding fade-down to filter container

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -5,33 +5,36 @@ import SearchIcon from '@mui/icons-material/Search';
 
 export const Filter = () => {
   return (
-    <div className="filter-container">
-      <div className="titles">
-        <h1>A partir de R$ xx,xx</h1>
-        <div className="title">
-          <a href="https://www.google.com.br/">Consutlas</a>
-          <a href="https://www.google.com.br/">Oftalmo</a>
-          <a href="https://www.google.com.br/">Imagem</a>
-          <a href="https://www.google.com.br/">Cardiológico</a>
-          <a href="https://www.google.com.br/">Laboratório</a>
-        </div>
-      </div>
-
-      <div className="filter">
-        <div className="filter-input">
-          <input type="text" placeholder="Do que você precisa?">
-            
-          </input>
-          <div className="search-icon">
-            <SearchIcon />
+    <div data-aos="fade-down">
+      <div className="filter-container">
+        <div className="titles">
+          <h1>A partir de R$ xx,xx</h1>
+          <div className="title">
+            <a href="https://www.google.com.br/">Consutlas</a>
+            <a href="https://www.google.com.br/">Oftalmo</a>
+            <a href="https://www.google.com.br/">Imagem</a>
+            <a href="https://www.google.com.br/">Cardiológico</a>
+            <a href="https://www.google.com.br/">Laboratório</a>
           </div>
         </div>
 
-        <div className="filter-right">
-          <h1>Preço</h1>
-          <h1>Localidade</h1>
+        <div className="filter">
+          <div className="filter-input">
+            <input type="text" placeholder="Do que você precisa?">
+              
+            </input>
+            <div className="search-icon">
+              <SearchIcon />
+            </div>
+          </div>
+
+          <div className="filter-right">
+            <h1>Preço</h1>
+            <h1>Localidade</h1>
+          </div>
         </div>
       </div>
     </div>
+    
   )
 }

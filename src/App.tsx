ac862926a04c2012d5d5267e1b6a028f5dfa59a8
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-05-01 09:11:12 -0300

    feat: intro section has been added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 // Aos
 import Aos from 'aos';
 import 'aos/dist/aos.css'
-import { Filter } from './components/Filter';
+import { About } from './components/About';
 
 function App() {
   useEffect(() => {
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <Header />
       <Intro />
-      <Filter />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/index.tsx
@@ -0,0 +1,50 @@
+// Assets
+import aboutImg from '../../assets/intro.png';
+
+// Styles
+import './styles.css';
+
+// MUI Icons
+import AdsClickIcon from '@mui/icons-material/AdsClick';
+import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
+import GpsFixedIcon from '@mui/icons-material/GpsFixed';
+import WavesIcon from '@mui/icons-material/Waves';
+
+export const About = () => {
+  return (
+    <div className="bg-container">
+      <div className="about-container">
+        <div className="left-side">
+          <img src={aboutImg} alt="about" />
+        </div>
+        <div className="right-side">
+          <h1>Com os melhores profissionais</h1>
+          <p>Facilitaremos sua busca para encontrar o profissional, clínica ou laboratório perfeito para os cuidados com a sua saúde, sem precisar se preocupar com o preço.</p>
+
+          <div className="info-about--container">
+            <div className="box">
+              <AdsClickIcon />
+              <h1>Pontual</h1>
+              <p>Consultas presenciais com preços baixos</p>
+            </div>
+            <div className="box">
+              <ElectricBoltIcon />
+              <h1>Rápido</h1>
+              <p>Consultas presenciais com preços baixos</p>
+            </div>
+            <div className="box">
+              <GpsFixedIcon />
+              <h1>Localidade</h1>
+              <p>Consultas presenciais com preços baixos</p>
+            </div>
+            <div className="box">
+              <WavesIcon />
+              <h1>Novas tecnologias</h1>
+              <p>Consultas presenciais com preços baixos</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

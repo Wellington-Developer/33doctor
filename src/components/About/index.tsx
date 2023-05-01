9bf61c8924b831bcd16ea3f18a3923e824aa6a90
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-05-01 09:13:17 -0300

    style: adding a pink dot on intro container

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -18,7 +18,7 @@ export const About = () => {
           <img src={aboutImg} alt="about" />
         </div>
         <div className="right-side">
-          <h1>Com os melhores profissionais</h1>
+          <h1>Com os melhores profissionais<span>.</span></h1>
           <p>Facilitaremos sua busca para encontrar o profissional, clínica ou laboratório perfeito para os cuidados com a sua saúde, sem precisar se preocupar com o preço.</p>
 
           <div className="info-about--container">

0278dc1f56cbd7e82d67645018fc3c050e356388
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-05-05 13:23:27 -0300

    fix: header in content removed

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,5 @@
 import { About } from "./About"
 import { Banner } from "./Banner"
-import { Header } from "./Header"
 import { Intro } from "./Intro"
 import { SlideServices } from "./SlideServices"
 import { Testimonials } from "./Testimonials"
